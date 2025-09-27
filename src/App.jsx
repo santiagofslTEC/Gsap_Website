df9cde3c97882dfa6a811bd5ec19c3a9953652c7
Santiago Fernandez Somellera <a01646946@tec.mx>
2025-09-27 14:14:41 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import gsap from 'gsap';
+import {ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+    return (
+        <div className="flex-center h-[100vh]">
+            <h1 className="text-3xl text-indigo-300">Hello GSAP!</h1>
+        </div>
+    )
+}
+export default App
+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
+
